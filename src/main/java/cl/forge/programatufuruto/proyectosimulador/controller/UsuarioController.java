14e578bdfa44ce1d771ad0223e7e09aa724d6ffa
Rogelio Cifuentes <rogelio.cifuentes@gmail.com>
2019-01-17 13:00:48 -0300

    Todo andando

--- src/main/java/cl/forge/programatufuruto/proyectosimulador/controller/UsuarioController.java
+++ src/main/java/cl/forge/programatufuruto/proyectosimulador/controller/UsuarioController.java
@@ -20,27 +20,29 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
-    @CrossOrigin()
-    @RequestMapping("/registrar")
-    public void registrarUsuario(@RequestParam String rut, @RequestParam String password, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String correo) {
+    @CrossOrigin(origins="*")
+    @PostMapping("/registrar")
+    public boolean registrarUsuario(@RequestBody Usuario user) {
 
-        if (usuarioService.existeUsuarioPorId(rut)) {               //Compara el RUT ingresado y lo compara con la DB, si esque esta, arroja un mensaje que dice que esta en uso
+        if (usuarioService.existeUsuarioPorId(user.getRut())) {               //Compara el RUT ingresado y lo compara con la DB, si esque esta, arroja un mensaje que dice que esta en uso
             System.out.println("Rut actualmente en uso");
+            return false;
         }else{                                                             //Si no esta, crea un objeto usuario, cuyo rol se define por el admin
-            Usuario usuario = new Usuario(rut, password, nombre, apellido, correo, new Date());
+            Usuario usuario = new Usuario(user.getRut(), user.getPassword(), user.getNombre(), user.getApellido(), user.getCorreo(), new Date());
             usuarioService.guardarUsuario(usuario);
             System.out.println("Usuario registrado exitosamente");
+            return true;
         }
     }
 
-    @CrossOrigin()
-    @RequestMapping("/login")
-    public boolean login(@RequestParam("rut") String rut, @RequestParam("password") String password){
+    @CrossOrigin(origins="*")
+    @PostMapping("/login")
+    public boolean login(@RequestBody Usuario user){
 
-        List<Usuario> lista = usuarioService.validador(rut,password);                       //Se hace una consulta a la DB con los datos ingresados y se crea una lista con el resultado
+        List<Usuario> lista = usuarioService.validador(user.getRut(), user.getPassword());                       //Se hace una consulta a la DB con los datos ingresados y se crea una lista con el resultado
 
         if(lista.size() != 0){                                                              //Si la lista es distinta de 0, es porque hay alguien con ese RUT y esa PASSWORD en la DB
-            String nombre = usuarioService.obtenerUsuario(rut).getNombre();                 //La consulta obtenerUsuario, me trae un objeto Usuario por su rut, del cual se le extrae el nombre
+            String nombre = usuarioService.obtenerUsuario(user.getRut()).getNombre();                 //La consulta obtenerUsuario, me trae un objeto Usuario por su rut, del cual se le extrae el nombre
             System.out.println("Bienvenido "+nombre);
             return true;
         }else{
